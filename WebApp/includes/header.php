5f588af0bf0b1b3c92d1f0d7066231bef27a7d15
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-03-22 16:26:53 +0000

    Added: Includes folder
    Contains:
    Footer.php
    Header.php
    These contains the websites header and footer to save on code space.

---
+++ WebApp/includes/header.php
@@ -0,0 +1,47 @@
+<header class="header">
+    <div class="top-right-container">
+        <div class="top-right-content">
+            <!--Search Bar-->
+            <div class="searchbar-mn">
+                <div class="searchbar-content-mn">
+                    <form action="action_page.php">
+                        <input type="text" placeholder="Search.." name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+            <!--Sign out/switch user test-->
+            <div class="log-mn log-mb">
+                <div class="dropdown-container-mn">
+                    <button class="dropbtn-mn">Logged in:</button>
+                    <div class="dropdown-cont-mn">
+                        <input type="submit" value="logout">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="header-center">
+        <div class="kfh-logo">
+            <img src="images/kfhm-logo.png" alt="kfh-logo">
+        </div>
+    </div>
+    <div class="header-left">
+        <!--Left Nav Bar-->
+        <div class="sidenav-mn sidenav-mb">
+            <div class="dropdown-container-mn">
+                <button class="dropbtn-mn">
+                    <img src="https://s2.svgbox.net/hero-outline.svg?color=green&ic=menu" width="32" height="32">
+                </button>
+                <div class="dropdown-cont-mn" style="left:0;">
+                    <ul class="dropdown-sidenav-ul">
+                        <li class="dropdown-sidenav-li"><a href="index.php">File Upload</a></li>
+                        <li class="dropdown-sidenav-li"><a href="admin.html">Admin</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</header>
