571e6c18fec05013bbd9d5e1b58cdc3b841adb55
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-03-22 16:40:05 +0000

    Updated: Database file
    Update:
    Added the most up to date state of the database with the most current tables.

--- WebApp/database/edrm_stage3.sql
+++ WebApp/database/edrm_stage3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2024 at 09:59 PM
+-- Generation Time: Mar 22, 2024 at 05:38 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,14 +34,62 @@ CREATE TABLE `files` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `finance_manager`
+--
+
+CREATE TABLE `finance_manager` (
+  `ID` int(11) NOT NULL,
+  `Permission` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_roles`
+--
+
+CREATE TABLE `job_roles` (
+  `id` int(11) NOT NULL,
+  `role_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager_permission`
+--
+
+CREATE TABLE `manager_permission` (
+  `ID` int(11) NOT NULL,
+  `Permission_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manager_permission`
+--
+
+INSERT INTO `manager_permission` (`ID`, `Permission_name`) VALUES
+(1, 'Permission 1'),
+(2, 'Permission 2'),
+(3, 'Permission 3'),
+(4, 'Permission 4'),
+(5, 'Permission 5'),
+(6, 'Permission 6');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `staff`
 --
 
 CREATE TABLE `staff` (
-  `staff_id` int(11) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `job_role` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `staffid` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `job_role_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -49,20 +97,75 @@ CREATE TABLE `staff` (
 --
 
 --
+-- Indexes for table `finance_manager`
+--
+ALTER TABLE `finance_manager`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Permission` (`Permission`);
+
+--
+-- Indexes for table `job_roles`
+--
+ALTER TABLE `job_roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `manager_permission`
+--
+ALTER TABLE `manager_permission`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
-  ADD UNIQUE KEY `staff_id` (`staff_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `job_role_id` (`job_role_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `finance_manager`
+--
+ALTER TABLE `finance_manager`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `job_roles`
+--
+ALTER TABLE `job_roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `manager_permission`
+--
+ALTER TABLE `manager_permission`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `finance_manager`
+--
+ALTER TABLE `finance_manager`
+  ADD CONSTRAINT `finance_manager_ibfk_1` FOREIGN KEY (`Permission`) REFERENCES `manager_permission` (`ID`);
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`job_role_id`) REFERENCES `job_roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
