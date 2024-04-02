f2566374fd3db9a18ff3a6995d13cfef37b9da95
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-02 12:58:43 +0100

    Create nav.php
    Created:
    PHP containing Nav bar

---
+++ WebApp/includes/nav.php
@@ -0,0 +1,8 @@
+<nav class="topnav-mn">
+    <ul>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="docs.php">DOCUMENTS</a></li>
+        <li><a href="archives.html">ARCHIVES</a></li>
+        <li><a href="admin.php">ADMIN</a></li>
+    </ul>
+</nav>
