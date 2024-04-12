e4d5c41884c0c7dee79bbf72556445b4e9344667
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-12 23:18:10 +0100

    Create docs.php

---
+++ WebApp/docs.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EDRM</title>
+    <link rel="stylesheet" href="css/mobile.css" />
+    <link rel="stylesheet" href="css/desktop.css" media="only screen and (min-width : 720px)" />
+</head>
+
+<body>
+    <div class="container-mn container-mb">
+        <div class="navbar-mn navbar-mb">
+            <?php
+            include 'includes/nav.php';
+            ?>
+        </div>
+        <div class="header-mn header-mb">
+            <?php
+            include "includes/header.php";
+            ?>
+        </div>
+        <div class="file-searchbar">
+            <input type="text" class="search-bar" placeholder="Search for file">
+        </div>
+
+        <div class="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>File</th>
+                        <th>Owner</th>
+                        <th>Date Created</th>
+                        <th>Audit Trail Modified</th>
+                        <th>Department</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+                    <tr>
+                        <td>#12121212</td>
+                        <td>Bob</td>
+                        <td>2024-03-12 10:30 AM</td>
+                        <td>BOB</td>
+                        <td>BOB</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+</body>
+
+</html>
