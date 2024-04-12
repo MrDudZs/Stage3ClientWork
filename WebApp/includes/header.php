ee4e4ea916c7a5b543a070f44fa1b124f765ad7c
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-12 23:15:55 +0100

    Update header.php

--- WebApp/includes/header.php
+++ WebApp/includes/header.php
@@ -1,6 +1,27 @@
 <header class="header">
     <div class="top-right-container">
         <div class="top-right-content">
+            <!--Sign out/switch user test-->
+            <div class="log-mn log-mb">
+                <div class="dropdown-container-mn">
+                    <button class="dropbtn-mn">
+                        <?php
+                        session_start();
+                        // Check if the user is logged in
+                        if (isset($_SESSION['username'])) {
+                            echo "Logged in:" . $_SESSION['username'];
+                        } else {
+                            echo "Login";
+                        }
+                        ?>
+                    </button>
+                    <div class="dropdown-cont-mn">
+                        <form action="logout.php" method="post">
+                            <input type="submit" value="logout">
+                        </form>
+                    </div>
+                </div>
+            </div>
             <!--Search Bar-->
             <div class="searchbar-mn">
                 <div class="searchbar-content-mn">
@@ -8,15 +29,6 @@
                         <input type="text" placeholder="Search.." name="search">
                         <button type="submit"><i class="fa fa-search"></i></button>
                     </form>
-                </div>
-            </div>
-            <!--Sign out/switch user test-->
-            <div class="log-mn log-mb">
-                <div class="dropdown-container-mn">
-                    <button class="dropbtn-mn">Logged in:</button>
-                    <div class="dropdown-cont-mn">
-                        <input type="submit" value="logout">
-                    </div>
                 </div>
             </div>
         </div>
@@ -36,8 +48,9 @@
                 </button>
                 <div class="dropdown-cont-mn" style="left:0;">
                     <ul class="dropdown-sidenav-ul">
-                        <li class="dropdown-sidenav-li"><a href="index.php">File Upload</a></li>
-                        <li class="dropdown-sidenav-li"><a href="admin.html">Admin</a></li>
+                        <li class="dropdown-sidenav-li"><a href="dashboard.php">Dashboard</a></li>
+                        <li class="dropdown-sidenav-li"><a href="docupload.php">File Upload</a></li>
+                        <li class="dropdown-sidenav-li"><a href="admin.php">Admin</a></li>
                     </ul>
                 </div>
             </div>
