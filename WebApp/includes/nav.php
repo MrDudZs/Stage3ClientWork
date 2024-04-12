8b5aae78f5de506fd611a958804e7265c4f606ca
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-12 23:15:58 +0100

    Update nav.php

--- WebApp/includes/nav.php
+++ WebApp/includes/nav.php
@@ -2,7 +2,7 @@
     <ul>
         <li><a href="index.php">HOME</a></li>
         <li><a href="docs.php">DOCUMENTS</a></li>
-        <li><a href="archives.html">ARCHIVES</a></li>
+        <li><a href="archives.php">ARCHIVES</a></li>
         <li><a href="admin.php">ADMIN</a></li>
     </ul>
 </nav>
