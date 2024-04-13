9b43a0960c0882d0015d17b30fe2824adae165bb
Naz2833 <nasiral2833@gmail.com>
2024-04-13 21:37:42 +0100

    changed style sheet/ start of footer

--- WebApp/index.php
+++ WebApp/index.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EDRM</title>
     <link rel="stylesheet" href="css/mobile.css" />
+<<<<<<< Updated upstream
     <link rel="stylesheet" href="css/mattsdesktoptest.css" media="only screen and (min-width : 601px)" />
+=======
+    <link rel="stylesheet" href="css/mattsdesktoptest.css." media="only screen and (min-width : 601px)" />
+>>>>>>> Stashed changes
 
 </head>
 
