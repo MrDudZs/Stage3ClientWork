5fc25de5ffc83f02e1f087f5bee8674928d678b6
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-13 21:03:38 +0100

    Create add-users.php

---
+++ WebApp/add-users.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document: Add User</title>
+    <link rel="stylesheet" href="css/mobile.css" />
+    <link rel="stylesheet" href="css/mattsdesktoptest.css" media="only screen and (min-width : 601px)" />
+    <?php
+    session_start()
+        ?>
+</head>
+
+<body>
+    <div class="container-mn">
+        <div class="navbar-cont-mn">
+            <?php
+            include "includes/nav.php";
+            ?>
+        </div>
+        <div class="header-cont-mn">
+            <?php
+            include "includes/header.php";
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
