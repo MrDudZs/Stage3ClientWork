354e9bf8c3d72b2a9553854617e13073f8182f18
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-13 21:04:04 +0100

    Update docupload.php

--- WebApp/docupload.php
+++ WebApp/docupload.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EDRM</title>
+    <title>Upload</title>
     <link rel="stylesheet" href="css/mobile.css" />
     <link rel="stylesheet" href="css/mattsdesktoptest.css" media="only screen and (min-width : 601px)" />
-
+    <?php
+        session_start()
+    ?>
 </head>
 
 <body>
@@ -24,13 +27,15 @@
         </div>
 
         <div class="indexTestUpload">
-            <form action="upload.php" method="post" enctype="multipart/form-data">
-                <input type="file" name="file">
-                <select name="doc_severity">
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+                <input type="file" name="file" required>
+                <select name="doc_severity" required>
                     <option value="LOW">LOW</option>
                     <option value="MEDIUM">MEDIUM</option>
                     <option value="HIGH">HIGH</option>
                 </select>
+                <input type="text" name="class" placeholder="Type of document" required>
+                <input type="date" name="doc_due" required>
                 <input type="submit" value="Upload" style="margin-top: 10px;">
             </form>
         </div>
