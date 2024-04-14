25bb44aba6fadfbb8d9aeed994c11bcac5434e73
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-14 21:02:25 +0100

    Changed: Style and Docupload.php
    Changes:
    
    I have just changed the the style for the docsupload.php due to it conflicting with other inputs
    
    Added:
    (docupload.php)
    - Added classes to style
    (maincss)
    - Added the classes for style within docupload.php

--- WebApp/docupload.php
+++ WebApp/docupload.php
@@ -1,3 +1,6 @@
+<?php
+session_start()
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,9 +11,6 @@
     <title>Upload</title>
     <link rel="stylesheet" href="css/mobile.css" />
     <link rel="stylesheet" href="css/mattsdesktoptest.css" media="only screen and (min-width : 601px)" />
-    <?php
-        session_start()
-    ?>
 </head>
 
 <body>
@@ -29,7 +29,7 @@
 
         <div class="indexTestUpload">
             
-        <form action="upload.php" method="post" enctype="multipart/form-data">
+        <form class="docup-form" action="upload.php" method="post" enctype="multipart/form-data">
                 <input type="file" name="file" required>
                 <select name="doc_severity" required>
                     <option value="LOW">LOW</option>
@@ -38,7 +38,7 @@
                 </select>
                 <input type="text" name="class" placeholder="Type of document" required>
                 <input type="date" name="doc_due" required>
-                <input type="submit" value="Upload" style="margin-top: 10px;">
+                <input class="docup-submit" type="submit" value="Upload" style="margin-top: 10px;">
             </form>
         </div>
     </div>
