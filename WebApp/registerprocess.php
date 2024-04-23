9d7c30022b74b254848163c5b0839dd264439706
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-23 09:23:12 +0100

    Update registerprocess.php
    Re-done creating user to prevent php injection

--- WebApp/registerprocess.php
+++ WebApp/registerprocess.php
@@ -1,41 +1,36 @@
 <?php
-
 session_start();
-
-// Connection
 include 'php/config.php';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $first_name = $_POST['first_name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $dob = $_POST['dob'];
+    $password = $_POST['password'];
+    $job_role = isset($_POST['job_role']) ? $_POST['job_role'] : ""; 
+    $permission_id = isset($_POST['permission_id']) ? $_POST['permission_id'] : ""; 
+    $department_id = isset($_POST['department_id']) ? $_POST['department_id'] : ""; 
+
+    // Hash password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Set is_suspended to false
+    $is_suspended = false;
+
+    $sql = "INSERT INTO staff (first_name, surname, email, dob, password, is_suspended, job_role, permission_id, department_id) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssssiisi", $first_name, $surname, $email, $dob, $hashed_password, $is_suspended, $job_role, $permission_id, $department_id);
+    
+    if ($stmt->execute()) {
+        echo "User registered successfully";
+        header("Location: login.php");
+        exit();
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+    
+    $stmt->close();
 }
-
-$first_name = $_POST['first_name'];
-$surname = $_POST['surname'];
-$email = $_POST['email'];
-$dob = $_POST['dob'];
-$password = $_POST['password'];
-$job_role = isset($_POST['job_role']) ? $_POST['job_role'] : ""; 
-$permission_id = isset($_POST['permission_id']) ? $_POST['permission_id'] : ""; 
-$department_id = isset($_POST['department_id']) ? $_POST['department_id'] : ""; 
-
-
-// Hash password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-// Set is_suspended to false
-$is_suspended = false;
-
-// SQL query to insert user into the database
-$sql = "INSERT INTO staff (first_name, surname, email, dob, password, is_suspended, job_role, permission_id, department_id) 
-VALUES ('$first_name', '$surname', '$email', '$dob', '$hashed_password', '$is_suspended', '$job_role', '$permission_id', '$department_id')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "User registered successfully";
-    header("Location: login.php");
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-// Close connection
-$conn->close();
